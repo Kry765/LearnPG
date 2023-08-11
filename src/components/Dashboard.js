649e100a1c34d005763788d7c9734a155bd9e197
Kry765 <krzysztofkleka91@gmail.com>
2023-08-11 23:08:47 +0200

    ..

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,22 @@
+import '../scss/_dashboard.scss'
+import { RxHamburgerMenu } from 'react-icons/rx'
+
 const Dashboard = () => {
 	return (
-		<div>
-			<p>Hello World!</p>
+		<div className='dashboard'>
+			<div className='wrapper dashboard__menu-items'>
+				<div>
+					<RxHamburgerMenu className='wrapper dashboard__menu-burger' />
+				</div>
+				<div className='dashboard__menu-item--menu-logo'>logo</div>
+				<div className='dashboard__menu-item'>Support</div>
+				<div className='dashboard__menu-item'>Setting</div>
+				<div className='dashboard__menu-item'>User</div>
+			</div>
+			<div className='dashboard__main wrapper'>
+				<div className='dashboard__section-box--theory wrapper'>Ucz się teorii</div>
+				<div className='dashboard__section-box wrapper'>Ucz się praktyka</div>
+			</div>
 		</div>
 	)
 }
