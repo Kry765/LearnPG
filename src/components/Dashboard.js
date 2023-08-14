47d704933fc60ede1aa19886dc8c244a0e2c73df
Kry765 <krzysztofkleka91@gmail.com>
2023-08-14 08:47:50 +0200

    ..

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import '../scss/_dashboard.scss'
 import { RxHamburgerMenu } from 'react-icons/rx'
+import Praktyka from '../image/praktyka.jpg'
+import Teoria from '../image/teoria.jpg'
 
 const Dashboard = () => {
 	return (
@@ -14,8 +16,16 @@ const Dashboard = () => {
 				<div className='dashboard__menu-item'>User</div>
 			</div>
 			<div className='dashboard__main wrapper'>
-				<div className='dashboard__section-box--theory wrapper'>Ucz się teorii</div>
-				<div className='dashboard__section-box wrapper'>Ucz się praktyka</div>
+				<div className='dashboard__section-box'>
+					<img src={Teoria} alt='Teoria' className='dashboard__section-box-image' />
+					<div className='dashboard__section-box-opacity'></div>
+				</div>
+				<div className='dashboard__section-box'>
+					<img src={Praktyka} alt='Praktyka' className='dashboard__section-box-image' />
+					<div className='dashboard__section-box-opacity wrapper'>
+						<p>Teoria</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
