6cd66544a57668778a68ea984ac8f9f69e834194
Kry765 <krzysztofkleka91@gmail.com>
2023-09-27 23:12:03 +0200

    edit dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,34 +1,20 @@
 import '../scss/_dashboard.scss'
-import { RxHamburgerMenu } from 'react-icons/rx'
-import Praktyka from '../image/praktyka.jpg'
-import Teoria from '../image/teoria.jpg'
 
 const Dashboard = () => {
 	return (
 		<div className='dashboard'>
-			<div className='wrapper dashboard__menu-items'>
-				<div>
-					<RxHamburgerMenu className='wrapper dashboard__menu-burger' />
-				</div>
-				<div className='dashboard__menu-item--menu-logo'>logo</div>
-				<div className='dashboard__menu-item'>Support</div>
-				<div className='dashboard__menu-item'>Setting</div>
-				<div className='dashboard__menu-item'>User</div>
-			</div>
-			<div className='dashboard__main wrapper'>
-				<div className='dashboard__section-box'>
-					<img src={Teoria} alt='Teoria' className='dashboard__section-box-image' />
-					<div className='dashboard__section-box-opacity wrapper'>
-						<p className='dashboard__section-box--subtitle'>Teoria</p>
-					</div>
-				</div>
-				<div className='dashboard__section-box'>
-					<img src={Praktyka} alt='Praktyka' className='dashboard__section-box-image' />
-					<div className='dashboard__section-box-opacity wrapper'>
-						<p className='dashboard__section-box--subtitle'>Praktyka</p>
-					</div>
+			<div className='dashboard__left-menu'>
+				<div className='dashboard__menu-items'>
+					<div className='dashboard__menu-item'>Logo</div>
+					<div className='dashboard__menu-item'>Materiały</div>
+					<div className='dashboard__menu-item'>Pomoc</div>
+					<div className='dashboard__menu-item'>Ustawienia</div>
+					<div className='dashboard__menu-item'>Rozpocznij nauke</div>
+					<div className='dashboard__menu-item'>Wyloguj się</div>
 				</div>
 			</div>
+			<div className='dashboard__welcome-page'>Witaj test</div>
+			{/* <div>Rozpocznij nauke</div> */}
 		</div>
 	)
 }
