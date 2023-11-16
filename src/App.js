7bf8795af36a94e85035f5ec7e89bc8df0036663
Kry765 <krzysztofkleka91@gmail.com>
2023-11-17 00:05:54 +0100

    change mail and delete accound

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Main from './frontend/components/Main'
+import Help from './frontend/components/Help'
 import Reset from './frontend/components/Reset'
 import Register from './frontend/components/Register'
 import Login from './frontend/components/Login'
@@ -6,8 +7,10 @@ import Dashboard from './frontend/components/Dashboard'
 import Learn from './frontend/components/Learn'
 import Exam from './frontend/components/Exam'
 import Settings from './frontend/components/Settings'
+import ResetEmail from './frontend/components/ResetEmail'
 import ResultCloseQuestion from './frontend/components/ResultCloseQuestion'
 import Lab from './frontend/components/Lab'
+import DeleteUser from './frontend/components/DeleteUser'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -25,6 +28,9 @@ function App() {
 					<Route path='/Dashboard/Learn/Lab' element={<Lab />} />
 					<Route path='/Dashboard/Exam' element={<Exam />} />
 					<Route path='/Dashboard/Results' element={<ResultCloseQuestion />} />
+					<Route path='/Dashboard/Help' element={<Help />} />
+					<Route path='/Dashboard/Settings/ResetEmail' element={<ResetEmail />} />
+					<Route path='/Dashboard/Settings/DeleteUser' element={<DeleteUser />} />
 				</Routes>
 			</Router>
 		</div>
