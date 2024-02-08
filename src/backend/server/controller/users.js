be142a78266169ffbdb78beebb47920b6a17e775
Kry765 <krzysztofkleka91@gmail.com>
2024-02-08 17:40:14 +0100

    ...

--- src/backend/server/controller/users.js
+++ src/backend/server/controller/users.js
@@ -1,16 +1,16 @@
 const User = require('../models/user')
-const express = require('express')
-const getUser = express.Router()
 
-getUser.get('/test', (req, res, next) => {
-	User.findAll()
-		.then(users => {
-			res.status(200).json({ users: users })
-		})
-		.catch(err => {
-			console.log(err)
-			res.status(500).json({ error: 'Internal server error' })
-		})
-})
+const getUser = app => {
+	app.get('/getUser', (req, res, next) => {
+		User.findAll()
+			.then(users => {
+				res.status(200).json({ users: users })
+			})
+			.catch(err => {
+				console.log(err)
+				res.status(500).json({ error: 'Internal server error' })
+			})
+	})
+}
 
 module.exports = getUser
--- src/frontend/components/AdminPage.js
+++ src/frontend/components/AdminPage.js
@@ -4,10 +4,9 @@ import { checkCorrectEmail, checkEmptyInput, checkRepeatPassword, checkStrongPwd
 
 export default function AdminPage() {
 	const [user_email, set_user_email] = useState('')
-	const [repeat_pwd, set_repeat_pwd] = useState('')
 	const [user_pwd, set_user_pwd] = useState('')
-	// const handleCheckEmail = checkCorrectEmail(user_email)
-	// const handleCheckEmptyInput = checkEmptyInput(user_email, user_pwd, repeat_pwd)
+	const [outputUser, setOutputUser] = useState('')
+	const API_URL = 'http://localhost:4000'
 
 	const checkEmail = () => {
 		return checkCorrectEmail(user_email)
@@ -15,6 +14,17 @@ export default function AdminPage() {
 
 	const checkInput = () => {
 		return checkEmptyInput(user_email, user_pwd)
+	}
+
+	const getUser = () => {
+		axios
+			.get(API_URL + '/getUser')
+			.then(response => {
+				setOutputUser(response.data.users)
+			})
+			.catch(error => {
+				console.error('Błąd zapytania:', error)
+			})
 	}
 
 	const addUser = async e => {
@@ -31,10 +41,9 @@ export default function AdminPage() {
 		}
 
 		const formData = { user_email, user_pwd }
-		const API_URL = 'http://localhost:4000/create'
 
 		try {
-			const res = await axios.post(API_URL, formData, {
+			const res = await axios.post(API_URL + '/create', formData, {
 				headers: {
 					'Content-Type': 'application/json',
 				},
@@ -52,28 +61,56 @@ export default function AdminPage() {
 		}
 	}
 
+	const deleteUser = () => {}
+
 	return (
-		<div>
-			<h2>Dodaj konto</h2>
-			<form onSubmit={addUser}>
-				<input
-					type='text'
-					placeholder='adres-email'
-					value={user_email}
-					onChange={event => {
-						set_user_email(event.target.value)
-					}}
-				/>
-				<input
-					type='text'
-					placeholder='hasło'
-					value={user_pwd}
-					onChange={event => {
-						set_user_pwd(event.target.value)
-					}}
-				/>
-				<button type='submit'>Dodaj konto</button>
-			</form>
+		<div className='flex'>
+			<div>
+				<div>
+					<h2>Pobierz wszystkich użytkowników</h2>
+
+					<button onClick={getUser()}>Pobierz userów</button>
+				</div>
+				<h2>Dodaj konto</h2>
+				<form onSubmit={addUser}>
+					<input
+						type='text'
+						placeholder='adres-email'
+						value={user_email}
+						onChange={event => {
+							set_user_email(event.target.value)
+						}}
+					/>
+					<input
+						type='text'
+						placeholder='hasło'
+						value={user_pwd}
+						onChange={event => {
+							set_user_pwd(event.target.value)
+						}}
+					/>
+					/>
+					<button type='submit'>Dodaj konto</button>
+				</form>
+				<div>
+					<h2>Skasuj konto</h2>
+					<form onSubmit={deleteUser}>
+						<input
+							type='text'
+							placeholder='adres-email'
+							value={user_email}
+							onChange={event => {
+								set_user_email(event.target.value)
+							}}
+						/>
+						<button type='submit'>Skasuj konto</button>
+					</form>
+				</div>
+			</div>
+			<div>
+				<h2>Lista użytkowników:</h2>
+				<div>{Array.isArray(outputUser) && outputUser.map((user, index) => <p key={index}>{user.email}</p>)}</div>
+			</div>
 		</div>
 	)
 }
