aab81dee6488afc924962a446064fd526cb902de
Kry765 <krzysztofkleka91@gmail.com>
2024-02-10 17:48:17 +0100

    ...

---
+++ src/backend/server/controller/addNewTopic.js
@@ -0,0 +1,18 @@
+const Topic = require('../models/topic')
+
+const addNewTopic = app => {
+	app.post('/addnewtopic', async (req, res) => {
+		try {
+			const { topicName, topicDescription } = req.body
+			const addTopic = await Topic.create({
+				topic_name: topicName,
+				topic_description: topicDescription,
+			})
+			res.status(201).json(addTopic)
+		} catch (error) {
+			res.status(500).json({ error: 'Wystąpił błąd podczas dodawania nowego tematu.' })
+		}
+	})
+}
+
+module.exports = addNewTopic
--- src/frontend/components/AdminPage.js
+++ src/frontend/components/AdminPage.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { checkCorrectEmail, checkEmptyInput, checkRepeatPassword, checkStrongPwd } from '../../backend/guard/Script'
 
 export default function AdminPage() {
 	const [user_email, set_user_email] = useState('')
+	const [topicName, setTopicName] = useState('')
+	const [topicDescription, setTopicDescription] = useState('')
 	const [user_pwd, set_user_pwd] = useState('')
-	const [outputUser, setOutputUser] = useState('')
+	const [users, setUsers] = useState([])
+
 	const API_URL = 'http://localhost:4000'
+
+	useEffect(() => {
+		getUser()
+	}, [])
 
 	const checkEmail = () => {
 		return checkCorrectEmail(user_email)
@@ -16,11 +23,21 @@ export default function AdminPage() {
 		return checkEmptyInput(user_email, user_pwd)
 	}
 
+	const handleNewTopic = async e => {
+		e.preventDefault()
+		const formTopics = { topicName, topicDescription }
+		const res = await axios.post(API_URL + '/addnewtopic', formTopics, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+	}
+
 	const getUser = () => {
 		axios
 			.get(API_URL + '/getUser')
 			.then(response => {
-				setOutputUser(response.data.users)
+				setUsers(response.data.users)
 			})
 			.catch(error => {
 				console.error('Błąd zapytania:', error)
@@ -69,7 +86,7 @@ export default function AdminPage() {
 				<div>
 					<h2>Pobierz wszystkich użytkowników</h2>
 
-					<button onClick={getUser()}>Pobierz userów</button>
+					{/* <button onClick={getUser()}>Pobierz userów</button> */}
 				</div>
 				<h2>Dodaj konto</h2>
 				<form onSubmit={addUser}>
@@ -89,7 +106,7 @@ export default function AdminPage() {
 							set_user_pwd(event.target.value)
 						}}
 					/>
-					/>
+
 					<button type='submit'>Dodaj konto</button>
 				</form>
 				<div>
@@ -106,10 +123,44 @@ export default function AdminPage() {
 						<button type='submit'>Skasuj konto</button>
 					</form>
 				</div>
+				<div>
+					{/*ADD NEW TOPICS*/}
+					<h2>Dodaj nowy dział</h2>
+					<form onSubmit={handleNewTopic}>
+						<input
+							type='text'
+							placeholder='Nazwa działu'
+							onChange={event => {
+								setTopicName(event.target.value)
+							}}
+						/>
+						<textarea
+							placeholder='dodaj treść'
+							onChange={event => {
+								setTopicDescription(event.target.value)
+							}}
+						></textarea>
+						<button type='submit'>Dodaj nowe zagadnienie</button>
+					</form>
+				</div>
 			</div>
 			<div>
 				<h2>Lista użytkowników:</h2>
-				<div>{Array.isArray(outputUser) && outputUser.map((user, index) => <p key={index}>{user.email}</p>)}</div>
+				<div>
+					<div className='flex justify-space-around'>
+						<div>ID</div>
+						<div>E-mail</div>
+					</div>
+					{users.map((user, index) => (
+						<ul key={index}>
+							<div className='flex justify-space-around'>
+								<div>{user.user_id}</div>
+								<div>{user.user_email}</div>
+							</div>
+						</ul>
+					))}
+				</div>
+				<h2>Lista działów:</h2>
 			</div>
 		</div>
 	)
